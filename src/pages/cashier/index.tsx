9c5554764cc179686029b37bd08fb30f1b21e15d
hejuxin <cuobingfeicuo@126.com>
2024-04-09 17:59:03 +0800

    feat: 收银台

--- src/pages/cashier/index.tsx
+++ src/pages/cashier/index.tsx
@@ -5,11 +5,13 @@ import { Suspense, useEffect, useMemo, useState } from "react";
 import { getPageQuery } from "@/utils/utils";
 import { fetchMerchantChannelQueryMerchantChannelPage, fetchMerchantChannelQueryMerchantChannelDetail } from "@/services/channel/list";
 import { useRequest } from "@umijs/max";
-import { Card, InputNumber, Radio, Spin } from "antd";
+import { Button, Card, Form, InputNumber, Radio, Spin } from "antd";
+import { fetchOrderCreateOrder } from "@/services/order";
 
 const Cashier = () => {
   const [] = useState();
   const queryParams = getPageQuery();
+  const [form] = Form.useForm();
   const { channelId } = queryParams;
   const { data: channelInfo = {}, run: getInfo, loading } = useRequest(fetchMerchantChannelQueryMerchantChannelDetail, {
     manual: true
@@ -21,7 +23,22 @@ const Cashier = () => {
         id: channelId
       })
     }
-  }, [channelId])
+  }, [channelId]);
+
+
+  useEffect(() => {
+    if (channelInfo.id) {
+      const values: Record<string, any> = {
+        payType: Object.values(payTypeEnum)[0].value
+      }
+      if (channelInfo.amountType === 1) {
+        values.orderAmount = channelInfo.amountConfig;
+      }
+
+      // todo
+      form.setFieldsValue(values)
+    }
+  }, [channelInfo]);
 
   // console.log(channelList, 'ccc')
 
@@ -32,81 +49,82 @@ const Cashier = () => {
 
   // console.log(channelInfo, 'channelInfo')
 
+  const handleSubmit = async () => {
+    form.validateFields().then(res => {
+      console.log(res, 'res')
+      fetchOrderCreateOrder({
+        ...res,
+        channelId: channelInfo.channelId
+      }).then(res => {
+        console.log(res, 'res')
+      })
+    })
+  }
+
   return (
     <PageContainer title={<></>}>
       <Card>
-        <ProForm
-          style={{
-            margin: 'auto',
-            width: 'fit-content'
-          }}
-          layout="vertical"
-        // onFinish={onFinish}
-        >
-          {
-            !loading && (
-              <>
-                {
-                  channelInfo!.amountType === 1 && (
-                    <ProFormRadio.Group
-                      initialValue={channelInfo!.amountConfig}
-                      label='支付金额'
-                      name='amountConfig'
-                      disabled
-                      options={[
-                        {
-                          label: channelInfo!.amountConfig,
-                          value: String(channelInfo!.amountConfig)
-                        }
-                      ]}
-                    />
-                  )
-                }
-                {
-                  channelInfo!.amountType === 3 && (
-                    <ProFormRadio.Group
-                      label='支付金额'
-                      name='amountConfig'
-                      options={channelInfo!.amountConfig?.split('.').map((item: number) => (
-                        {
-                          label: item,
-                          value: item
-                        }
-                      ))}
-                    />
-                  )
-                }
-                {
-                  channelInfo!.amountType === 2 && (
-                    <ProFormDigit
-                      label='支付金额'
-                      name="amountConfig"
-                      placeholder="请输入"
-                      min={channelInfo!.amountConfig?.split('-')[0]}
-                      max={channelInfo!.amountConfig?.split('-')[1]}
-                      width="xs"
-                      fieldProps={{
-                        formatter: (value) => `¥${value || channelInfo!.amountConfig?.split('-')[0]}`,
-                        parser: (value) => Number(value ? value.replace('¥', '') : '0'),
-                      }}
-                    />
-                  )
-                }
-                <ProFormRadio.Group
-                  label='支付方式'
-                  name='payType'
-                  options={Object.values(payTypeEnum).map(item => (
+        <Form form={form}>
+          <Form.Item name='orderAmount' rules={[{ required: true }]}>
+            {
+              channelInfo!.amountType === 1 && (
+                <Radio.Group
+                  value={channelInfo!.amountConfig}
+                  disabled
+                  options={[
                     {
-                      label: item.text,
-                      value: item.value
+                      label: channelInfo!.amountConfig,
+                      value: String(channelInfo!.amountConfig)
+                    }
+                  ]}
+                >
+                  {/* <Radio value={channelInfo!.amountConfig}>{channelInfo!.amountConfig}</Radio> */}
+                </Radio.Group>
+              )
+            }
+            {
+              channelInfo!.amountType === 3 && (
+                <Radio.Group
+                  options={channelInfo!.amountConfig?.split('.').map((item: number) => (
+                    {
+                      label: item,
+                      value: item
                     }
                   ))}
                 />
-              </>
-            )
-          }
-        </ProForm>
+              )
+            }
+            {
+              channelInfo!.amountType === 2 && (
+                <InputNumber
+                  placeholder="请输入"
+                  min={channelInfo!.amountConfig?.split('-')[0]}
+                  max={channelInfo!.amountConfig?.split('-')[1]}
+                  width="xs"
+                // fieldProps={{
+                //   formatter: (value) => `¥${value || channelInfo!.amountConfig?.split('-')[0]}`,
+                //   parser: (value) => Number(value ? value.replace('¥', '') : '0'),
+                // }}
+                />
+              )
+            }
+          </Form.Item>
+          <Form.Item label='支付方式' name='payType' rules={[{ required: true }]}>
+            <Radio.Group
+              options={Object.values(payTypeEnum).map(item => (
+                {
+                  label: item.text,
+                  value: item.value
+                }
+              ))}
+            />
+          </Form.Item>
+        </Form>
       </Card>
+      <div className={styles.footer}>
+        <div className={styles.amount}>需支付：{form.getFieldValue('orderAmount') ? <><span>{form.getFieldValue('orderAmount')}</span>元</> : '-'}</div>
+        <Button onClick={handleSubmit}>立即支付</Button>
+      </div>
     </PageContainer>
   )
 }
